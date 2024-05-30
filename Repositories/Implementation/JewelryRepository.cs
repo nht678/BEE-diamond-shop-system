f7e6ac40d1d8160d0d4d3e22a2313cb7c58d4d94
anhkiet307 <kietlulk6969@gmail.com>
2024-05-30 15:11:09 +0700

    add repository

---
+++ Repositories/Implementation/JewelryRepository.cs
@@ -0,0 +1,34 @@
+﻿using BusinessObjects.Models;
+using DAO;
+using Repositories.Interface;
+
+namespace Repositories.Implementation
+{
+    public class JewelryRepository : IJewelryRepository
+    {
+        public async Task<int> Create(Jewelry entity)
+        {
+            return await JewelryDAO.Instance.CreateJewelry(entity);
+        }
+
+        public async Task<int> Delete(int id)
+        {
+            return await JewelryDAO.Instance.DeleteJewelry(id);
+        }
+
+        public async Task<IEnumerable<Jewelry?>?> GetAll()
+        {
+            return await JewelryDAO.Instance.GetJewelries();
+        }
+
+        public async Task<Jewelry?> GetById(int id)
+        {
+            return await JewelryDAO.Instance.GetJewelryById(id);
+        }
+
+        public async Task<int> Update(int id, Jewelry entity)
+        {
+            return await JewelryDAO.Instance.UpdateJewelry(id, entity);
+        }
+    }
+}
