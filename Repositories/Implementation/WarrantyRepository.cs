b00d6a4e034c942c474b165f80306c6d939ed6f1
nht678 <thanhnhat2112@gmail.com>
2024-05-30 15:54:38 +0700

    add respository warranty

---
+++ Repositories/Implementation/WarrantyRepository.cs
@@ -0,0 +1,34 @@
+﻿using BusinessObjects.Models;
+using DAO;
+using Repositories.Interface;
+
+namespace Repositories.Implementation
+{
+    public class WarrantyRepository : IWarrantyRepository
+    {
+        public async Task<int> Create(Warranty entity)
+        {
+            return await WarrantyDAO.Instance.CreateWarranty(entity);
+        }
+
+        public Task<IEnumerable<Warranty>> Find(Func<Warranty, bool> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Warranty?>?> GetAll()
+        {
+            return await WarrantyDAO.Instance.GetWarranties();
+        }
+
+        public async Task<Warranty?> GetById(int id)
+        {
+            return await WarrantyDAO.Instance.GetWarrantyById(id);
+        }
+
+        public async Task<int> Update(int id, Warranty entity)
+        {
+            return await WarrantyDAO.Instance.UpdateWarranty(id, entity);
+        }
+    }
+}
