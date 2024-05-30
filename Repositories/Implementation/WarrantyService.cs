26431ba00b9cf6c9e385f8039a0608c5f32cb595
nht678 <thanhnhat2112@gmail.com>
2024-05-30 15:51:17 +0700

    add reponsitory warranty

---
+++ Repositories/Implementation/WarrantyService.cs
@@ -0,0 +1,41 @@
+﻿using BusinessObjects.Models;
+using Repositories.Interface;
+using Services.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Implementation
+{
+    public class WarrantyService(IWarrantyRepository warrantyRepository) : IWarrantyService
+    {
+        public IWarrantyRepository WarrantyRepository { get; } = warrantyRepository;
+
+        public Task<int> CreateWarranty(Warranty warranty)
+        {
+            return WarrantyRepository.Create(warranty);
+        }
+
+        public Task<int> DeleteWarranty(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Warranty?>?> GetWarranties()
+        {
+            return await WarrantyRepository.GetAll();
+        }
+
+        public async Task<Warranty?> GetWarrantyById(int id)
+        {
+            return await WarrantyRepository.GetById(id);
+        }
+
+        public async Task<int> UpdateWarranty(int id,Warranty warranty)
+        {
+            return await WarrantyRepository.Update(id,warranty);
+        }
+    }
+}
