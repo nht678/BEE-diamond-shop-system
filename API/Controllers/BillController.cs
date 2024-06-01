39fa5abcfa90608065f7e7d2a3c174912453428f
anhkiet307 <kietlulk6969@gmail.com>
2024-06-01 22:18:30 +0700

    bill

---
+++ API/Controllers/BillController.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using BusinessObjects.DTO;
+using Microsoft.AspNetCore.Mvc;
+using Services.Interface;
+
+namespace API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BillController(IBillService billService, IMapper mapper) : ControllerBase
+{
+    public IBillService BillService { get; } = billService;
+    public IMapper Mapper { get; } = mapper;
+
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        var bills = await BillService.GetAll();
+        return Ok(bills);
+    }
+    [HttpGet("{id}")]
+    public async Task<IActionResult> Get(int id)
+    {
+        var bill = await BillService.GetById(id);
+        if (bill == null)
+        {
+            return NotFound();
+        }
+        return Ok(bill);
+    }
+    [HttpPost]
+    public async Task<IActionResult> Create(BillDTO billDto)
+    {
+        var result = await BillService.Create(billDto);
+        return Ok(result);
+    }
+}
---
+++ Services/Implementation/BillService.cs
@@ -0,0 +1,32 @@
+﻿using BusinessObjects.DTO;
+using BusinessObjects.Models;
+using Repositories.Interface;
+using Services.Interface;
+
+namespace Services.Implementation
+{
+    public class BillService(IBillRepository billRepository) : IBillService
+    {
+        public IBillRepository BillRepository { get; } = billRepository;
+
+        public async Task<int> Create(BillDTO entity)
+        {
+            return await billRepository.Create(entity);
+        }
+
+        public async Task<Bill?> FindBillByCustomerId(int customerId)
+        {
+            return await billRepository.FindBillByCustomerId(customerId);
+        }
+
+        public async Task<IEnumerable<Bill?>?> GetAll()
+        {
+            return await billRepository.GetAll();
+        }
+
+        public async Task<Bill?> GetById(int id)
+        {
+            return await billRepository.GetById(id);
+        }
+    }
+}
