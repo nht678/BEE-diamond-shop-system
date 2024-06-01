7c07bbbbf3842b8ccf26b46ab4d6b75c653e8e0f
nht678 <thanhnhat2112@gmail.com>
2024-06-01 22:25:16 +0700

    fix

---
+++ DAO/BillJewelryDAO.cs
@@ -0,0 +1,30 @@
+﻿using BusinessObjects.Models;
+using DAO.Context;
+using DAO.Interfaces;
+using Microsoft.EntityFrameworkCore;
+namespace DAO
+{
+    public class BillJewelryDAO : Singleton<BillJewelryDAO>
+    {
+        private readonly JssatsV2Context _context;
+        public BillJewelryDAO()
+        {
+            _context = new JssatsV2Context();
+        }
+        public async Task<IEnumerable<BillJewelry?>?> GetBillJewelries()
+        {
+            return await _context.BillJewelries.ToListAsync();
+        }
+        public async Task<BillJewelry?> GetBillJewelryById(int id)
+        {
+            return await _context.BillJewelries.FindAsync(id);
+        }
+        public async Task<int> CreateBillJewelry(BillJewelry billJewelry)
+        {
+            var maxBillJewelryId = await _context.BillJewelries.MaxAsync(b => b.BillJewelryId);
+            billJewelry.BillJewelryId = maxBillJewelryId + 1;
+            _context.BillJewelries.Add(billJewelry);
+            return await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ Repositories/Interface/IBillRepository.cs
@@ -0,0 +1,9 @@
+﻿using BusinessObjects.DTO;
+using BusinessObjects.Models;
+using Repositories.Interface.GenericRepository;
+
+namespace Repositories.Interface;
+public interface IBillRepository : IReadRepository<Bill>, ICreateRepository<BillDTO>
+{
+    public Task<Bill?> FindBillByCustomerId(int customerId);
+}
